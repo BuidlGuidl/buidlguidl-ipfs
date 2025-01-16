00c8cc907426a0ee2ca3b9b5ad0896e744ed69a2
azf20 <azfuller20@gmail.com>
2025-01-16 00:15:18 +0000

    fix up app

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -4,15 +4,9 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">BuidlGuidl IPFS</h1>
         <ol className="list-inside list-decimal text-sm text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-4">
-            Simple IPFS cluster setup (Coming soon)
-          </li>
-          <li className="mb-4">
-            Upload library (Coming soon)
-          </li>
-          <li className="mb-4">
-            Server guide (Coming soon)
-          </li>
+          <li className="mb-4">Simple IPFS cluster setup (Coming soon)</li>
+          <li className="mb-4">Upload library (Coming soon)</li>
+          <li className="mb-4">Server guide (Coming soon)</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -24,15 +18,14 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://docs.ipfs.tech"
+            href="https://github.com/azf20/buidlguidl-ipfs"
             target="_blank"
             rel="noopener noreferrer"
           >
-            IPFS Docs
+            Github
           </a>
         </div>
       </main>
-    
     </div>
   );
 }
