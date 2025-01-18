d14d7bcceebcb9805c3800a3b5af1fd3ae6ab8ca
azf20 <azfuller20@gmail.com>
2025-01-18 20:20:59 +0000

    chore: remove experiments, tidy

--- packages/bgipfs-cli/commands/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Description: Stop the IPFS cluster services
-
-stop_command() {
-    logger "INFO" "Stopping Docker containers..."
-    docker compose stop
-    logger "INFO" "Docker containers stopped"
-} 
