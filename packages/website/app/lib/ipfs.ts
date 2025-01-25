f66a77813ffb7fca60a5e835adfa3ff55fdba1fc
azf20 <azfuller20@gmail.com>
2025-01-25 22:37:02 +0000

    remove proxy url

--- packages/website/app/lib/ipfs.ts
+++ packages/website/app/lib/ipfs.ts
@@ -1,10 +1,6 @@
 import { createUploader } from "ipfs-uploader";
 import { headers } from 'next/headers';
 
-if (!process.env.IPFS_PROXY_URL) {
-  throw new Error("IPFS_PROXY_URL environment variable is not set");
-}
-
 export const pinningAuth = Buffer.from(
   `${process.env.IPFS_AUTH_USERNAME}:${process.env.IPFS_AUTH_PASSWORD}`
 ).toString("base64");
